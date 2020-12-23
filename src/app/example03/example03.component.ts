cca4ccce6a7b0bd2c7ef690c02ea86a3c5ce6b7b
Julio Perez <soyJulioLPJ@bitbucket.org>
2020-12-23 09:28:36 -0300

    Add example03 component (registration form)

--- src/app/example03/example03.component.ts
+++ src/app/example03/example03.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-example03',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class Example03Component implements OnInit {
+  miForm: FormGroup;
 
-  constructor() { }
+  get name() {
+    return this.miForm.get('name');
+  }
+
+  get email() {
+    return this.miForm.get('email');
+  }
+
+  get password() {
+    return this.miForm.get('password');
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.miForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  save() {
+    console.log('Form', this.miForm);
+    console.log('Value', this.miForm.value);
   }
 
 }
